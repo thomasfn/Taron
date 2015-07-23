d8e42796842c3e189760c2502c2e8971849d005e
Thomas Smith <thomasfn@fortfn.co.uk>
2015-07-23 22:05:00 +0100

    Preliminary work on the translator

---
+++ Taron/Translator/ReflectionTranslator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Taron.Model;
+
+namespace Taron.Translator
+{
+    /// <summary>
+    /// Represents a translator capable of translating types via reflection
+    /// </summary>
+    public sealed class ReflectionTranslator : IModelTranslator
+    {
+        private TranslateOptions translateOpts;
+
+        /// <summary>
+        /// Initialises a new instance of the ReflectionTranslator class
+        /// </summary>
+        /// <param name="opts"></param>
+        public ReflectionTranslator(TranslateOptions opts)
+        {
+            translateOpts = opts;
+        }
+
+        /// <summary>
+        /// Gets the capability of this translator to translate the specified type
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public TranslateCapability CanTranslate(Type type)
+        {
+            if (type.IsPrimitive)
+                return TranslateCapability.None;
+            else
+                return TranslateCapability.CanSerialise | TranslateCapability.CanDeserialise;
+        }
+
+        /// <summary>
+        /// Serialises the specified object into a value node
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public ValueNode Serialise(object obj)
+        {
+            // TODO: Scan obj's type for all public fields (which aren't readonly) and public properties (with setters)
+            // Then, set them, recursing through translateOpts and hence other translators
+            return null;
+        }
+
+        /// <summary>
+        /// Deserialises the specified value node into a .NET object
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public object Deserialise(Type t, ValueNode node)
+        {
+            // TODO: This
+            return null;
+        }
+
+        /// <summary>
+        /// Populates the a .NET object with the specified value node
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="node"></param>
+        public void Deserialise(object obj, ValueNode node)
+        {
+            // TODO: This
+        }
+    }
+}
